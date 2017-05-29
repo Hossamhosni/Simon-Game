7f0d07594bc39b689176b67bf9957d901481be22
Hossam <jkasdhkjadas>
2017-05-29 20:46:39 +0200

    Added audio

--- script.js
+++ script.js
@@ -4,15 +4,12 @@
 
 	var pattern = [];
 	pattern[0] = Math.floor(Math.random() * (4-1));
-	var simonSoundsUrl = "https://s3.amazonaws.com/freecodecamp/";
-	var simonSounds = ["simonSound1.mp3", "simonSound2.mp3", "simonSound3.mp3", "simonSound4.mp3"];
 
 	// cache dom elements
 	var $blue  = $('#0');
 	var $red = $('#1');
 	var $yellow = $('#2');
 	var $green = $('#3');
-
 
 	var nextMoveIndex = 0;
 	var strict = false;
@@ -37,40 +34,44 @@
 				case 0:
 					setTimeout(function () {
 						$blue.css('backgroundColor', 'blue');
+						document.getElementById('blueAudio').play();
 					}, intervalTime);
 					intervalTime += 500;
 						setTimeout(function () {
 							var j = 0;
-							$blue.css('backgroundColor', '#094A8F')
+							$blue.css('backgroundColor', '#094A8F');
 						}, intervalTime);
 						intervalTime += 500;
 					break;
 				case 1:
 					setTimeout(function () {
 						$red.css('backgroundColor', 'red');
+						document.getElementById('redAudio').play();
 					}, intervalTime);
 					intervalTime += 500;
 
 						setTimeout(function () {
 							var j = 0;
-							$red.css('backgroundColor', '#9F0F17')
+							$red.css('backgroundColor', '#9F0F17');
 						}, intervalTime);
 						intervalTime += 500;
 					break;
 				case 2:
 					setTimeout(function () {
 						$yellow.css('backgroundColor', 'yellow');
+						document.getElementById('yellowAudio').play();
 					}, intervalTime);
 					intervalTime += 500;
 
 						setTimeout(function () {
-							$yellow.css('backgroundColor', '#CCA707')
+							$yellow.css('backgroundColor', '#CCA707');
 						}, intervalTime);
 						intervalTime += 500;
 					break;
 				case 3:
 					setTimeout(function () {
 						$green.css('backgroundColor', 'green');
+						document.getElementById('greenAudio').play();
 					},intervalTime);
 
 					intervalTime += 500;
@@ -101,6 +102,8 @@
 		for (var i = 0 ; i < highestTimeoutId ; i++) {
 		    clearTimeout(i);
 		}
+		score = 0;
+		$('#count').text('Count: 0');
 	};
 
 	$('.fa').click(function () {
@@ -117,38 +120,39 @@
 		}
 	});
 
-	$('.button').click(function () {
+	$('.button').mousedown(function () {
 		if (!start) return;
 		var wrong = false;
 		if ($(this).attr('id') === '0') {
 			$blue.css('backgroundColor', 'blue');
+			document.getElementById('blueAudio').play();
 			if (pattern[nextMoveIndex] !== 0) {
 				wrongAnswer();
 				wrong = true;
 			}
 		} else if ($(this).attr('id') === '1') {
+			document.getElementById('redAudio').play();
+			$red.css('backgroundColor', 'red');
 			if (pattern[nextMoveIndex] !== 1) {
-				$red.css('backgroundColor', 'red');
 				wrongAnswer();
 				wrong = true;
 			}
 		} else if ($(this).attr('id') === '2') {
-			if (pattern[nextMoveIndex] !== 2) {
+				document.getElementById('yellowAudio').play();
 				$yellow.css('backgroundColor', 'yellow');
+			if (pattern[nextMoveIndex] !== 2) {
 				wrongAnswer();
 				wrong = true;
 			}
 		} else if ($(this).attr('id') === '3') {
+			document.getElementById('greenAudio').play();
+			$green.css('backgroundColor', 'green');
 			if (pattern[nextMoveIndex] !== 3) {
-				$green.css('backgroundColor', 'green');
 				wrongAnswer();
 				wrong = true;
 			}
 		}
-		$blue.css('backgroundColor', '#094A8F');
-		$red.css('backgroundColor', '#9F0F17');
-		$yellow.css('backgroundColor', '#CCA707');
-		$green.css('backgroundColor', '#00A74A');
+
 		if (!wrong)
 			nextMoveIndex++;
 		if (nextMoveIndex === pattern.length)
@@ -156,11 +160,21 @@
 			score++;
 			$('#count').text('Count: ' + score);
 			pattern.push(Math.floor(Math.random() * (5-1)));
-			var time = nextRound();
+			setTimeout(function () {
+				var time = nextRound();
+			}, 500);
+
 			setTimeout(function () {
 				$('.button').css('pointerEvents', 'auto');
-			}, time);
+			}, 1000000 * 2);
 		}
+	});
+
+	$('.button').mouseup(function () {
+		$blue.css('backgroundColor', '#094A8F');
+		$red.css('backgroundColor', '#9F0F17');
+		$yellow.css('backgroundColor', '#CCA707');
+		$green.css('backgroundColor', '#00A74A');
 	});
 
 	$('#strict').click(function () {
